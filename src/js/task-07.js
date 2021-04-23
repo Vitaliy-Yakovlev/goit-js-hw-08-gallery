22bf02340046ad77b452e75f347b2def96a6dd35
Vitaliy <vitaliy-yakovlev@ukr.net>
2021-04-23 17:51:12 +0300

    Подготовка к ДЗ

--- src/js/task-07.js
+++
@@ -1,8 +0,0 @@
-const inputRef = document.querySelector('#font-size-control');
-const textRef = document.querySelector('#text');
-
-inputRef.addEventListener('input', fontSizeControl);
-
-function fontSizeControl() {
-  textRef.style.fontSize = inputRef.value + 'px';
-}
