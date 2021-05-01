fe75cf4b9629fc65760acf4b7da7b79c92b131a2
Vitaliy <vitaliy-yakovlev@ukr.net>
2021-05-01 19:34:24 +0300

    Сделал перелистывание фото влево и право

--- src/hw-08.js
+++ src/hw-08.js
@@ -18,11 +18,15 @@ refs.buttonClose.addEventListener('click', removeClassModal);
 
 refs.divOverlay.addEventListener('click', onCloseMauseModal);
 
+const lightBox = document.querySelector('.lightbox__image');
+
 window.addEventListener('keydown', onCloseKeyModal);
+window.addEventListener('keydown', nextImageLeft);
+window.addEventListener('keydown', nextImageRight);
 
 function listMarkupImage(linkImages) {
   return linkImages
-    .map(({ preview, original, description }) => {
+    .map(({ preview, original, description }, i) => {
       return `<li class="gallery__item">
   <a
     class="gallery__link"
@@ -89,4 +93,40 @@ function onCloseKeyModal(e) {
   if (e.key === 'Escape') {
     removeClassModal();
   }
+}
+
+function nextImageLeft(e) {
+  if (e.code !== 'ArrowLeft') {
+    return;
+  }
+
+  let currentImageIndex = linkImages.findIndex(
+    element => element.original === lightBox.getAttribute('src'),
+  );
+
+  if (currentImageIndex === 0) {
+    currentImageIndex = linkImages.length;
+  }
+
+  const prevImageUrl = linkImages[currentImageIndex - 1].original;
+
+  lightBox.setAttribute('src', prevImageUrl);
+}
+
+function nextImageRight(e) {
+  if (e.code !== 'ArrowRight') {
+    return;
+  }
+
+  let currentImageIndex = linkImages.findIndex(
+    element => element.original === lightBox.getAttribute('src'),
+  );
+
+  if (currentImageIndex === linkImages.length - 1) {
+    currentImageIndex = -1;
+  }
+
+  const nextImageUrl = linkImages[currentImageIndex + 1].original;
+
+  lightBox.setAttribute('src', nextImageUrl);
 }
